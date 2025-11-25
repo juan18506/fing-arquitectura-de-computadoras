7d4dbcf568af61d56cd4600c55b6ec9b2870dc08
Juan Alves Pedreira <112975672+juan18506@users.noreply.github.com>
2025-11-24 21:17:46 -0300

    Update ej3.c

--- 8086-recursion/ej3.c
+++ 8086-recursion/ej3.c
@@ -1,5 +1,3 @@
-#include <stdbool.h>
-
 struct Nodo {
     short dato;
     unsigned char hijoIzq, hijoDer;
@@ -8,12 +6,12 @@ struct Nodo arbol[256];
 
 bool ej3(short buscado, unsigned char i) {
     if (i == 0) {
-        return false;
+        return 0;
     } else if (buscado == arbol[i].dato) {
-        return true;
-    } else if (buscado < arbol[i].dato) {
-        return ej3(buscado, arbol[i].hijoIzq);
-    } else if (buscado > arbol[i].dato) {
-        return ej3(buscado, arbol[i].hijoDer);
+        return 1;
+    } else {
+        char estaIzq = ej3(buscado, arbol[i].hijoIzq);
+        char estaDer = ej3(buscado, arbol[i].hijoDer);
+        return estaIzq | estaDer;
     }
 }
